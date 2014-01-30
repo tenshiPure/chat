ab50995bba397d79d4985df81937d0a111f94e27
tenshiPure <user.ryo@gmail.com>
2014-01-30 12:40:09 +0900

    create : views.message / create MessageListView.

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,24 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from message.views import MessageListView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'chat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+
+	url(r'^login/$',
+		'django.contrib.auth.views.login',
+		{'template_name' : 'common/login.html'}
+	),
+
+	url(r'^logout/$',
+		'django.contrib.auth.views.logout',
+		{'template_name' : 'common/logout.html'}
+	),
+
+	url(r'^message/$',
+		MessageListView.as_view()
+	),
 )
--- message/views.py
+++ message/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from django.views.generic import ListView
 
-# Create your views here.
+from message.models import Message
+
+class MessageListView(ListView):
+
+	template_name = 'message/index.html'
+
+	def get_queryset(self):
+		return Message.objects.all()
